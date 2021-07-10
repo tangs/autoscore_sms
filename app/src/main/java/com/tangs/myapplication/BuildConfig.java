4a47c79dfdcc8ca724710bf9babf79509a8de976
tangs <tangsong1988kaka@gmail.com>
2021-07-10 16:32:25 +0800

    update.

--- app/src/main/java/com/tangs/myapplication/BuildConfig.java
+++ app/src/main/java/com/tangs/myapplication/BuildConfig.java
@@ -3,5 +3,5 @@ package com.tangs.myapplication;
 public class BuildConfig {
     // 缅甸2-1只显示自己
     public static boolean IS_MIANDIAN_2_1 = false;
-    public static boolean IS_MIANDIAN_3_1 = true;
+    public static boolean IS_MIANDIAN_3_1 = false;
 }
